3208bd5674713cc4539a10f95940c9795105c7bb
SiddhantJha317 <siddhantjha365@gmail.com>
2023-01-18 15:53:51 +0800

    push debug fixes with comments

--- DebuggingExercises/DebugThree4.java
+++ DebuggingExercises/DebugThree4.java
@@ -1,17 +1,18 @@
-// This program calculates tutition bills as credits times rate per credit hour
+// https://github.com/SiddhantJha317/cp2406_farrell8_ch03
+// This program calculates tuition bills , in terms of credits per hour
 public class DebugThree4
 {
    public static void main(String args[])
    {
-      int myCredits = 13
-      int yourCredits = 17
-      double rate = 75.84
+      int myCredits = 13;
+      int yourCredits = 17;
+      double rate = 75.84;
       System.out.println("My tuition:");
       tuitionBill(myCredits, rate);
       System.out.println("Your tuition:");
-      tuitionBill(myCredits, rate);
+      tuitionBill(yourCredits, rate);
    }
-   public static void tuitionBill(double r)
+   public static void tuitionBill(int c, double r)
    {
       System.out.println("Total due " + (r*c));
    }
